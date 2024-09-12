58be9836b130d7ef1b804e6b9ad5bffcb7938a89
Pranav Arora <aurorapranav187@gmail.com>
2024-09-12 20:04:59 -0230

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_BLOG)
+
+  return (
+    <>
+     <h1>A blog app with Appwrite</h1>
+    </>
+  )
+}
+
+export default App

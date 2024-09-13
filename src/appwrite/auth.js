649ee32a700082dc1dee5b65abab5cd100461c45
Pranav Arora <aurorapranav187@gmail.com>
2024-09-12 23:09:31 -0230

    Add Appwrite Auth Service

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -12,6 +12,52 @@ export class AuthService {
         this.account = new Account(this.client);
     }
 
+    async createAccount({ email, password, name }) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.loginAccount({ email, password })
+            }
+            else return userAccount
+        } catch (error) {
+            throw error;
+        }
+
+    }
+
+    async loginAccount({ email, password }) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error;
+        }
+
+        // eslint-disable-next-line no-unreachable
+        return null
+
+    }
+
+    async logoutAccount() {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+
+        }
+    }
+
 
 }
 

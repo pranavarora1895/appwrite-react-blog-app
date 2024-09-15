052db84f478323a0601759498f10714a2b3fe6f0
Pranav Arora <aurorapranav187@gmail.com>
2024-09-15 10:43:14 -0230

    Add Signup and Login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import './App.css'
-import conf from './conf/conf'
+import { useDispatch } from 'react-redux'
+import { useEffect, useState } from 'react'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import {Header, Footer} from './components/index'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(conf.appwriteEndpoint)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout())
+        }
+      })
+      .finally(setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className="w-full block">
+        <Header/>
+        <main>
+          TODO <Outlet />
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "postcss/lib/container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutButton";
+
+export { Header, Footer, Container, Logo, LogoutButton }

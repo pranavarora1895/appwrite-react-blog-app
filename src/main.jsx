bfd634313d57be4838ee4481d349313141c12c79
Pranav Arora <aurorapranav187@gmail.com>
2024-09-15 20:08:22 -0230

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import AllPosts from './pages/AllPosts.jsx'
 import AddPost from './pages/AddPost.jsx'
 import EditPost from './pages/EditPost.jsx'
 import Post from './pages/Post.jsx'
-
+import { Analytics } from "@vercel/analytics/react"
 
 const router = createBrowserRouter([
   {
@@ -78,7 +78,8 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}/>
+      <Analytics />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
   ,

edec19b9ab71d9f36c1b6aa4e2e609eedc04c9f3
Pranav Arora <aurorapranav187@gmail.com>
2024-10-10 20:50:49 -0230

    Align login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,22 +29,14 @@ const Login = () => {
 
 
     return (
-        <div className='flex items-center justify-center w-full'>
+        <div className='flex items-center justify-center w-full p-8'>
             <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+            <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
                 <span className="inline-block w-full max-w-[100px]">
                     <Logo width="100%" />
                 </span>
             </div>
-            <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
-            <p className="mt-2 text-center text-base text-black/60">
-                Don&apos;t have any account?&nbsp;
-                <Link
-                    to="/signup"
-                    className="font-medium text-primary transition-all duration-200 hover:underline"
-                >
-                    Sign Up
-                </Link>
-            </p>
+           
             {error && <p className='text-red-500 text-center'>{error}</p>}
 
             <form onSubmit={handleSubmit(login)} className='mt-8'>
@@ -72,6 +64,15 @@ const Login = () => {
                         className='w-full'
                     >Sign In</Button>
                 </div>
+                <p className="mt-2 text-base text-black/60">
+                Don&apos;t have any account?&nbsp;
+                <Link
+                    to="/signup"
+                    className="font-medium text-primary transition-all duration-200 hover:underline"
+                >
+                    Sign Up
+                </Link>
+            </p>
             </form>
 
         </div>
